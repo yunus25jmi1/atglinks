ea212c65e46ef4faa830b86c122d775f68190c2d
Maulik Patel <69646201+Maulikpatell@users.noreply.github.com>
2022-11-27 21:09:49 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $SOURCE_CODE ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/YourUsername/ATG-Shortener-V2.git /ATG-Shortener-V2
+  git clone https://github.com/YourUsername/atglinks.git /atglinks
 else
   echo "Cloning Custom Repo from $SOURCE_CODE "
-  git clone $SOURCE_CODE /ATG-Shortener-V2
+  git clone $SOURCE_CODE /atglinks
 fi
-cd /ATG-Shortener-V2
+cd /atglinks
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 -m main
