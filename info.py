a946c5eb270382c07c4fcd161c46568974f8072c
Yunus Ahmad <115855149+yunus25jmi1@users.noreply.github.com>
2023-02-03 19:53:24 +0530

    Update and rename sample_info.py to info.py

---
+++ info.py
@@ -0,0 +1,18 @@
+# Mandatory variables for the bot to start
+API_ID = 7703556
+API_HASH = '8341f9e3bfcf89d521685b1dd041555d'
+BOT_TOKEN = '5784228007:AAFHETpGYokNb1j1k-BdQeRf_H4HNSZU6p8'
+ADMINS = [1069593275]
+DATABASE_NAME = 'Url-Bypass-Bot'
+DATABASE_URL = 'mongodb+srv://yunus:yunus@url-bypass-bot.u7fjeij.mongodb.net/?retryWrites=true&w=majority'
+
+#  Optionnal variables
+CHANNELS = True
+CHANNEL_ID = [-1001351663304]
+FORWARD_MESSAGE = True
+SOURCE_CODE = 'https://github.com/maullikpatell/URL-Shortener-V2'
+BANNER_IMAGE = 'https://te.legra.ph/file/6d917e511c15ea7d35f33.jpg'
+WELCOME_IMAGE = 'https://te.legra.ph/file/6d917e511c15ea7d35f33.jpg'
+LINK_BYPASS = True
+HEROKU_API_KEY = ''
+HEROKU_APP_NAME = ''
